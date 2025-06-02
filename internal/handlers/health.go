a7c59268d561c72c80382ad72202bbe092161e49
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-06-02 16:09:54 -0700

    feature: add HealthHander method; add interface

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -1,9 +1,35 @@
 package handlers
 
-import "time"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
 type HealthResponse struct {
 	Status    string    `json:"status"`
 	Timestamp time.Time `json:"timestamp"`
+	Version   string    `json:"version"`
 	Uptime    string    `json:"uptime"`
+}
+
+func HealthHandler(server ServerProvider) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache, no-store")
+
+		response := HealthResponse{
+			Status:    "Healthy",
+			Timestamp: time.Now(),
+			Version:   server.GetVersion(),
+			Uptime:    server.GetUptime().String(),
+		}
+
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+type ServerProvider interface {
+	GetVersion() string
+	GetUptime() time.Duration
 }
