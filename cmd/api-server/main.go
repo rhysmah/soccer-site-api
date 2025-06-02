dbcecdf0bac2d2bd045652f13aa5cf44cd7406f7
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-06-02 14:24:32 -0700

    chore: small changes

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	fmt.Fprintf(w, "Welcome to the home page of soccer teams API server.\n")
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
-	log.Print("Staring server on :8080")
+	log.Print("Starting server on :8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
